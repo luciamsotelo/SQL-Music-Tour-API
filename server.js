3d1dff48809335a97711aff71b730a384a3ed863
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-01-28 15:14:06 -0800

    added tables

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const { Sequelize } = require ('sequelize')
 
+
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
